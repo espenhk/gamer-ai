cc50339358dc7a5c26dfb11e0b99df24dfc39440
Espen H. Kristensen <espenkristensen@gmail.com>
2026-04-27 18:23:16 +0200

    Cleanup

--- clients/base.py
+++
@@ -1,2 +0,0 @@
-"""Backward-compatibility shim. Symbols moved to games.tmnf.clients.base."""
-from games.tmnf.clients.base import Phase, PhaseAwareClient  # noqa: F401
--- clients/instruction_client.py
+++
@@ -1,2 +0,0 @@
-"""Backward-compatibility shim. Symbols moved to games.tmnf.clients.instruction_client."""
-from games.tmnf.clients.instruction_client import InstructionClient  # noqa: F401
